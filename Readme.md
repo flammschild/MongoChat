fd3a382e1ee6bdf0f87ca0647c7a7fdc7fa0ab2b
Manuel Batsching <flammschild@gmail.com>
2015-04-28 17:06:35 +0200

    Read replica set config from file

--- Readme.md
+++ Readme.md
@@ -3,22 +3,48 @@
 Preparation
 -----------
 
-Setting up a mongodb replication cluster for testing purposes on a Windows machine (for other operating systems change the dbpath accordingly) and create the MessageCollection. 
+Setting up a mongodb replication cluster for testing purposes on a Windows machine (192.168.200.131) and one Linux client (192.168.200.3) (for other operating systems change the dbpath accordingly) and create the MessageCollection. 
 
 ````bat
+
+   ## Auf 192.168.200.131
    mkdir \data\rs0-0
    mkdir \data\rs0-1
    mkdir \data\rs0-2
-   mkdir \data\rs0-3
-   
    mongod --port 27017 --dbpath /data/rs0-0 --replSet rs0 --smallfiles --oplogSize 128
    mongod --port 27018 --dbpath /data/rs0-1 --replSet rs0 --smallfiles --oplogSize 128
    mongod --port 27019 --dbpath /data/rs0-2 --replSet rs0 --smallfiles --oplogSize 128
-   
-   mongo --port 27017
-   rsconf = { _id: "rs0", members: [ { _id: 0, host: "192.168.200.131:27017" } ] }
+
+   ## Auf 192.168.200.3
+   mkdir -p \data\rs0-3
+   mongod --port 27020 --dbpath ~/data/rs0-3 --replSet rs0 --smallfiles --oplogSize 128
+   mongo --port 27020
+   rsconf = {
+	"_id" : "rs0",
+	"version" : 5,
+	"members" : [
+		{
+			"_id" : 0,
+			"host" : "192.168.200.131:27017",
+			"priority" : 0.5
+		},
+		{
+			"_id" : 1,
+			"host" : "192.168.200.131:27018",
+			"priority" : 0.5
+		},
+		{
+			"_id" : 2,
+			"host" : "192.168.200.131:27019",
+			"priority" : 0.5
+		},
+		{
+			"_id" : 3,
+			"host" : "192.168.200.3:27020"
+            "priority" : 1
+		}
+	]
+   }
    rs.initiate(rsconf)
-   rs.add("192.168.200.131:27018")
-   rs.add("192.168.200.131:27019")
    db.createCollection("messages", {capped:true, autoIndexId:false, size:100000, max:20})
 ````
--- src/main/java/mongochat/Main.java
+++ src/main/java/mongochat/Main.java
@@ -1,26 +1,41 @@
 package mongochat;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.ServerSocket;
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
-       
+
     Logger log = LoggerFactory.getLogger(Main.class);
 
-    List<ServerAddress> mongoReplicaSet = new ArrayList<ServerAddress>();
-    mongoReplicaSet.add(new ServerAddress("192.168.200.131", 27017));
-    mongoReplicaSet.add(new ServerAddress("192.168.200.131", 27018));
-    mongoReplicaSet.add(new ServerAddress("192.168.200.131", 27019));
+    // Read configuration for mongo replica set from config file. 
+    URI configFileUri = null;
+    try {
+      configFileUri = Main.class.getResource("/replica_config.json").toURI();
+    } catch (URISyntaxException e) {
+      log.error("URI of replica set config file is malformed.");
+      e.printStackTrace();
+    } catch (NullPointerException e) {
+      log.error("Cannot create URI of replica set config file. Does it exist?");
+    }
+    String jsonString = String.join("", Files.readAllLines(Paths.get(configFileUri)));
+    Type serverAddressListType = new TypeToken<List<ServerAddress>>(){}.getType();
+    List<ServerAddress> mongoReplicaSet = new Gson().fromJson(jsonString, serverAddressListType);
 
     // Open a ServerSocket so that we can abuse telnet as a chat client user interface.
     ServerSocket serverSocket = new ServerSocket(4321);
